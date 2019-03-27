4b8ebdd6a08b2b6d89f345ced19f897a3f91033e
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2019-03-27 05:49:43 -0400

    Expose ident APIs

--- provide/Ident.cs
+++ provide/Ident.cs
@@ -21,82 +21,82 @@ namespace provide
             });
         }
 
-        // // CreateApplication on behalf of the given API token
-        // func CreateApplication(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Post("applications", params)
-        // }
+        // CreateApplication on behalf of the given API token
+        public static async Task<(int, object)>CreateApplication(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Post("applications", args);
+        }
 
-        // // UpdateApplication using the given API token, application id and params
-        // func UpdateApplication(token, applicationID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("applications/%s", applicationID)
-        //     return InitIdent(token).Put(uri, params)
-        // }
+        // UpdateApplication using the given API token, application id and args
+        public static async Task<(int, object)>UpdateApplication(string token, string applicationID, Dictionary<string, object> args) {
+            var uri = String.Format("applications/{0}", applicationID);
+            return await InitIdent(token).Put(uri, args);
+        }
 
-        // // ListApplications retrieves a paginated list of applications scoped to the given API token
-        // func ListApplications(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Get("applications", params)
-        // }
+        // ListApplications retrieves a paginated list of applications scoped to the given API token
+        public static async Task<(int, object)>ListApplications(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Get("applications", args);
+        }
 
-        // // GetApplicationDetails retrives application details for the given API token and application id
-        // func GetApplicationDetails(token, applicationID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("applications/%s", applicationID)
-        //     return InitIdent(token).Get(uri, params)
-        // }
+        // GetApplicationDetails retrives application details for the given API token and application id
+        public static async Task<(int, object)>GetApplicationDetails(string token, string applicationID, Dictionary<string, object> args) {
+            var uri = String.Format("applications/{0}", applicationID);
+            return await InitIdent(token).Get(uri, args);
+        }
 
-        // // ListApplicationTokens retrieves a paginated list of application API tokens
-        // func ListApplicationTokens(token, applicationID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("applications/%s/tokens", applicationID)
-        //     return InitIdent(token).Get(uri, params)
-        // }
+        // ListApplicationTokens retrieves a paginated list of application API tokens
+        public static async Task<(int, object)>ListApplicationTokens(string token, string applicationID, Dictionary<string, object> args) {
+            var uri = String.Format("applications/{0}/tokens", applicationID);
+            return await InitIdent(token).Get(uri, args);
+        }
 
-        // // CreateApplicationToken creates a new API token for the given application ID.
-        // func CreateApplicationToken(token, applicationID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     params["application_id"] = applicationID
-        //     return InitIdent(token).Post("tokens", params)
-        // }
+        // CreateApplicationToken creates a new API token for the given application ID.
+        public static async Task<(int, object)>CreateApplicationToken(string token, string applicationID, Dictionary<string, object> args) {
+            args["application_id"] = applicationID;
+            return await InitIdent(token).Post("tokens", args);
+        }
 
-        // // CreateToken creates a new API token.
-        // func CreateToken(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Post("tokens", params)
-        // }
+        // CreateToken creates a new API token.
+        public static async Task<(int, object)>CreateToken(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Post("tokens", args);
+        }
 
-        // // ListTokens retrieves a paginated list of API tokens scoped to the given API token
-        // func ListTokens(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Get("tokens", params)
-        // }
+        // ListTokens retrieves a paginated list of API tokens scoped to the given API token
+        public static async Task<(int, object)>ListTokens(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Get("tokens", args);
+        }
 
-        // // GetTokenDetails retrieves details for the given API token id
-        // func GetTokenDetails(token, tokenID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("tokens/%s", tokenID)
-        //     return InitIdent(token).Get(uri, params)
-        // }
+        // GetTokenDetails retrieves details for the given API token id
+        public static async Task<(int, object)>GetTokenDetails(string token, string tokenID, Dictionary<string, object> args) {
+            var uri = String.Format("tokens/{0}", tokenID);
+            return await InitIdent(token).Get(uri, args);
+        }
 
-        // // DeleteToken removes a previously authorized API token, effectively deauthorizing future calls using the token
-        // func DeleteToken(token, tokenID string) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("tokens/%s", tokenID)
-        //     return InitIdent(token).Delete(uri)
-        // }
+        // DeleteToken removes a previously authorized API token, effectively deauthorizing future calls using the token
+        public static async Task<(int, object)>DeleteToken(string token, string tokenID) {
+            var uri = String.Format("tokens/{0}", tokenID);
+            return await InitIdent(token).Delete(uri);
+        }
 
-        // // CreateUser creates a new user for which API tokens and managed signing identities can be authorized
-        // func CreateUser(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Post("users", params)
-        // }
+        // CreateUser creates a new user for which API tokens and managed signing identities can be authorized
+        public static async Task<(int, object)>CreateUser(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Post("users", args);
+        }
 
-        // // ListUsers retrieves a paginated list of users scoped to the given API token
-        // func ListUsers(string token, Dictionary<string, object> params) (int, interface{}, error) {
-        //     return InitIdent(token).Get("users", params)
-        // }
+        // ListUsers retrieves a paginated list of users scoped to the given API token
+        public static async Task<(int, object)>ListUsers(string token, Dictionary<string, object> args) {
+            return await InitIdent(token).Get("users", args);
+        }
 
-        // // GetUserDetails retrieves details for the given user id
-        // func GetUserDetails(token, userID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("users/%s", userID)
-        //     return InitIdent(token).Get(uri, params)
-        // }
+        // GetUserDetails retrieves details for the given user id
+        public static async Task<(int, object)>GetUserDetails(string token, string userID, Dictionary<string, object> args) {
+            var uri = String.Format("users/{0}", userID);
+            return await InitIdent(token).Get(uri, args);
+        }
 
-        // // UpdateUser updates an existing user
-        // func UpdateUser(token, userID string, Dictionary<string, object> params) (int, interface{}, error) {
-        //     uri := fmt.Sprintf("users/%s", userID)
-        //     return InitIdent(token).Put(uri, params)
-        // }
+        // UpdateUser updates an existing user
+        public static async Task<(int, object)>UpdateUser(string token, string userID, Dictionary<string, object> args) {
+            var uri = String.Format("users/{0}", userID);
+            return await InitIdent(token).Put(uri, args);
+        }
     }
 }
