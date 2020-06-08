46b29d416f0d83f9587484f6be5fbc80035b83f9
Kyle Thomas <kyle@provide.services>
2020-06-08 07:56:27 -0400

    Update exposed vault api client

--- provide/Vault.cs
+++ provide/Vault.cs
@@ -7,10 +7,41 @@ namespace provide
     public class Vault: ApiClient
     {
 
-        public Vault(string token) : base(token) {}
+        const string DEFAULT_HOST = "vault.provide.services";
+        const string DEFAULT_PATH = "api/v1";
+        const string DEFAULT_SCHEME = "https";
+        const string HOST_ENVIRONMENT_VAR = "VAULT_API_HOST";
+        const string SCHEME_ENVIRONMENT_VAR = "VAULT_API_SCHEME";
+        const string PATH_ENVIRONMENT_VAR = "VAULT_API_PATH";
 
+        public Vault(string token) : base(token) {}
+        public Vault(string host, string path, string scheme, string token) : base(host, path, scheme, token) {}
+    
         public static Vault InitVault(string token) {
-            return new Vault(token);
+            Vault vault;
+            try {
+                vault = new Vault(token);
+            } catch {
+                string host = Environment.GetEnvironmentVariable(HOST_ENVIRONMENT_VAR);
+                string path = Environment.GetEnvironmentVariable(PATH_ENVIRONMENT_VAR);
+                string scheme = Environment.GetEnvironmentVariable(SCHEME_ENVIRONMENT_VAR);
+
+                if (host == null) {
+                    host = DEFAULT_HOST;
+                }
+
+                if (path == null) {
+                    path = DEFAULT_PATH;
+                }
+
+                if (scheme == null) {
+                    scheme = DEFAULT_SCHEME;
+                }
+
+                vault = new Vault(host, path, scheme, token);
+            }
+
+            return vault;
         }
         
         // CreateVault creates a new vault available within the key management service
@@ -25,21 +56,39 @@ namespace provide
             return await this.Get(uri, args);
         }
 
-        // ListVaultKeys retrieves a list of the symmetric keys and asymmetric key pairs secured within the key management service
+        // ListVaultKeys retrieves a list of the symmetric keys and asymmetric key pairs secured within the vault key management service
         public async Task<(int, string)>ListVaultKeys(string token, string vaultID, Dictionary<string, object> args) {
-            var uri = String.Format("vaults/{}", vaultID);
+            var uri = String.Format("vaults/{}/keys", vaultID);
             return await this.Get(uri, args);
         }
 
-        // CreateVault creates a new vault available within the key management service
+        // CreateVaultKey creates a new key available within the vault key management service
         public async Task<(int, string)>CreateVaultKey(string token, string vaultID, Dictionary<string, object> args) {
             var uri = String.Format("vaults/{}/keys", vaultID);
             return await this.Post(uri, args);
         }
 
-        // DeleteVaultKey permanently removes the specified key material from within the key management service
+        // DeleteVaultKey permanently removes the specified key material from within the vault key management service
         public async Task<(int, string)>DeleteVaultKey(string token, string vaultID, string keyID) {
             var uri = String.Format("vaults/{}/keys/{}", vaultID, keyID);
+            return await this.Delete(uri);
+        }
+
+        // ListVaultSecrets retrieves a list of the secrets secured within the vault
+        public async Task<(int, string)>ListVaultSecrets(string token, string vaultID, Dictionary<string, object> args) {
+            var uri = String.Format("vaults/{}/secrets", vaultID);
+            return await this.Get(uri, args);
+        }
+
+        // CreateVaultSecret creates a new secret within the vault
+        public async Task<(int, string)>CreateVaultSecret(string token, string vaultID, Dictionary<string, object> args) {
+            var uri = String.Format("vaults/{}/secrets", vaultID);
+            return await this.Post(uri, args);
+        }
+
+        // DeleteVaultSecret permanently removes the specified secret from the vault
+        public async Task<(int, string)>DeleteVaultSecret(string token, string vaultID, string secretID) {
+            var uri = String.Format("vaults/{}/secrets/{}", vaultID, secretID);
             return await this.Delete(uri);
         }
 
