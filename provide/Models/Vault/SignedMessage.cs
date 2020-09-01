8d3f535830baa9e36f22b0ded43bbac734161692
Kyle Thomas <kyle@provide.services>
2020-09-01 01:48:19 -0400

    Add verified flag to vault signed message model

--- provide/Models/Vault/SignedMessage.cs
+++ provide/Models/Vault/SignedMessage.cs
@@ -1,10 +1,17 @@
+using Newtonsoft.Json;
+
 namespace provide.Model.Vault
 {
     // FIXME: type name probably can be better
     public class SignedMessage: BaseModel
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Message { get; set; }
 
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Signature { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public bool Verified { get; set; }
     }
 }
