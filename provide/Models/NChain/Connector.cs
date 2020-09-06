cd906f1002a37cfb3eb1464051fee29a92f1a1e5
skos92 <skostic9242@gmail.com>
2020-09-06 19:21:48 +0200

    Add connector model

--- provide/Models/NChain/Connector.cs
+++ provide/Models/NChain/Connector.cs
@@ -1,8 +1,32 @@
+using System;
+using Newtonsoft.Json;
+
 namespace provide.Model.NChain
 {
     public class Connector: BaseModel
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid NetworkId { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid Applicationid { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid Organizationid { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Type { get; set; }
-        //FIXME: Empty model
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string AccessedAt { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Node[] Nodes { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public ConnectorDetails Details { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public LoadBalancer[] LoadBalancers { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public ConnectorConfig Config { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     }
 }
---
+++ provide/Models/NChain/ConnectorConfig.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace provide.Model.NChain
+{
+    public class ConnectorConfig
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Container { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, object> Credentials { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, object> Env { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid EngineId { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid ProviderId { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Region { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Role { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public Guid TargetId { get; set; }
+    }
+}
