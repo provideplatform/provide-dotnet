2aa157fbcb00be190c0f43fea0fe79e8adce22d0
jovanm94 <mr.jovan94@gmail.com>
2020-09-14 15:19:21 +0200

    Renaming Baseline.cs; Added Business Object DTO for the future (#5)
    * Clean up todos
    
    * Add connector model
    
    * Add connected entity type
    
    * Remove todos
    
    * Fix compile
    
    * Small ident test modfication
    
    * Cleanup
    
    * Cleanup
    
    * Test cleanup
    
    * Cleanup
    
    * rename baseline methods
    
    * add businessobjectdto and mapper
    
    Co-authored-by: skos92 <skostic9242@gmail.com>
    Co-authored-by: Jovan Milovanovic <jovanmilovanovic01.com>

---
+++ provide/Profiles/BusinessObjectProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using provide.DTOs;
+using provide.Model.NChain;
+
+namespace provide.Profiles
+{
+    public class BusinessObjectProfile : Profile
+    {
+        public BusinessObjectProfile()
+        {
+            CreateMap<ConnectedEntity, BusinessObjectDto>();
+            CreateMap<BusinessObjectDto, ConnectedEntity>();
+        }
+    }
+}
