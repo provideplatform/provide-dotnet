32f85c4fe03dd22f620f7a2d8af525eae5cc6e2e
skos92 <skostic9242@gmail.com>
2020-09-23 16:12:27 +0200

    Fix writing

--- provide/Services/MessageProtocolParser.cs
+++ provide/Services/MessageProtocolParser.cs
@@ -26,28 +26,28 @@ public static class MessageProtocolParser
         {
             using(BinaryWriter bw = new BinaryWriter(ms, Encoding.UTF8))
             {
-                bw.Write(msg.OpCode);
+                bw.Write(msg.OpCode.ToCharArray());
 
                 bw.Seek(5, SeekOrigin.Begin);
-                bw.Write(msg.Sender);
+                bw.Write(msg.Sender.ToCharArray());
 
                 bw.Seek(5 + 42, SeekOrigin.Begin);
-                bw.Write(msg.Recipient);
+                bw.Write(msg.Recipient.ToCharArray());
 
                 bw.Seek(5 + 42 + 42, SeekOrigin.Begin);
-                bw.Write(msg.Shield);
+                bw.Write(msg.Shield.ToCharArray());
 
                 bw.Seek(5 + 42 + 42 + 42, SeekOrigin.Begin);
-                bw.Write(msg.Identifier);
+                bw.Write(msg.Identifier.ToCharArray());
 
                 bw.Seek(5 + 42 + 42 + 42 + 36, SeekOrigin.Begin);
                 bw.Write(reservedBits);
 
                 bw.Seek(5 + 42 + 42 + 42 + 36 + reservedBits.Length, SeekOrigin.Begin);
-                bw.Write(msg.Signature);
+                bw.Write(msg.Signature.ToCharArray());
 
                 bw.Seek(5 + 42 + 42 + 42 + 36 + reservedBits.Length + 64, SeekOrigin.Begin);
-                bw.Write(msg.Type);
+                bw.Write(msg.Type.ToCharArray());
 
                 bw.Seek(5 + 42 + 42 + 42 + 36 + reservedBits.Length + 64 + 1, SeekOrigin.Begin);
                 bw.Write(msg.Payload);
@@ -64,14 +64,14 @@ public static class MessageProtocolParser
         {
             using (var br = new BinaryReader(ms, Encoding.UTF8))
             {
-                var opCode = br.ReadBytes(5).ToString();
+                var opCode = br.ReadBytes(5);
                 var sender = Encoding.UTF8.GetString(br.ReadBytes(42));
                 var recipient = Encoding.UTF8.GetString(br.ReadBytes(42));
                 var shield = Encoding.UTF8.GetString(br.ReadBytes(42));
                 var identifier = Encoding.UTF8.GetString(br.ReadBytes(36));
                 var reserved = br.ReadBytes(reservedSize);
                 var signature = Encoding.UTF8.GetString(br.ReadBytes(64));
-                var type = Encoding.UTF8.GetString(br.ReadBytes(1));
+                var type = br.ReadBytes(1);
                 var payload = br.ReadBytes(msg.Length - (5 + 42 + 42 + 42 + 36 + 64 + 1 + reservedSize));
 
                 return new Message
