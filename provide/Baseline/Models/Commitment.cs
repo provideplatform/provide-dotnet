0fcc97d230e017f74d8d2c77975e6f20a8300810
ognjen.kurtic@between.rs <ognjen.kurtic@between.rs>
2020-12-18 17:56:09 +0100

    Add docs for baseline model properties

--- provide/Baseline/Models/Commitment.cs
+++ provide/Baseline/Models/Commitment.cs
@@ -4,14 +4,49 @@ namespace provide.Baseline.Model
 {
     public class Commitment
     {
+        /// <summary>
+        /// Arbitrary data. 
+        /// </summary>
         public Dictionary<string, object> Metadata { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int Location { get; set; }
+
+        /// <summary>
+        /// Salt such that currentHash = H(data + salt). 
+        /// </summary>
         public string Salt { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int[] Proof { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string[] PublicInputs { get; set; }
+
+        /// <summary>
+        /// Address of participant who created the commitment. 
+        /// </summary>
         public string Sender { get; set; }
+
+        /// <summary>
+        /// Allows mapping from participant address -> signature. 
+        /// </summary>
         public Dictionary<string, string> Signatures { get; set; }
+
+        /// <summary>
+        /// Unix timestamp when the commitment was pushed. 
+        /// </summary>
         public long Timestamp { get; set; } // TODO: int or long
+
+        /// <summary>
+        /// Commitment value. 
+        /// </summary>
         public string Value { get; set; }
     }
 }
--- provide/Baseline/Models/Workflow.cs
+++ provide/Baseline/Models/Workflow.cs
@@ -4,12 +4,39 @@ namespace provide.Baseline.Model
 {
     public class Workflow
     {
+        /// <summary>
+        /// Reference to the underlying circuit.
+        /// </summary>
         public Circuit Circuit  { get; set; }
-        public Commitment[] commitments { get; set; }
+
+        /// <summary>
+        /// Commitments[0] is latest commitment (new commitments are prepended to array).
+        /// </summary>
+        public Commitment[] Commitments { get; set; }
+
+        /// <summary>
+        /// Subset of parties in a workgroup.
+        /// </summary>
         public Participant[] Participants { get; set; }
+
+        /// <summary>
+        /// Workflow identifier; should match circuit.id
+        /// </summary>
         public string Identifier  { get; set; }
+
+        /// <summary>
+        /// Shield contract address.
+        /// </summary>
         public string Shield  { get; set; }
+
+        /// <summary>
+        /// Map of model name to model representing the underlying domain model and its local persistent store (i.e. system of record).
+        /// </summary>
         public Dictionary<string, Model> Persistence { get; set; } // TODO: Verify type
+
+        /// <summary>
+        /// Arbitrary workflow metadata.
+        /// </summary>
         public Dictionary<string, object> Metadata { get; set; }
     }
 }
