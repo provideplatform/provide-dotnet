0ba48487968ecfa10464aceee425a89dd6018e1b
ognjen.kurtic@between.rs <ognjen.kurtic@between.rs>
2021-01-11 17:38:47 +0100

    Add explicit nats connection exceptions catch when connection

--- provide/Services/NatsClient.cs
+++ provide/Services/NatsClient.cs
@@ -54,7 +54,7 @@ namespace provide
             {
                 this.connection = new ConnectionFactory().CreateConnection(opts);
             }
-            catch (Exception ex)
+            catch (Exception ex) when (ex is NATSNoServersException || ex is NATSConnectionException)
             {
                 System.Diagnostics.Debug.WriteLine($"Failed to establish nats connection: {ex}");
                 throw ex;
